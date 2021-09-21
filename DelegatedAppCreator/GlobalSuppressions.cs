e639e6b2692d75bb64fc364bbfa7d69f56275438
Mustafa Zengin <muzengin@microsoft.com>
2021-09-21 16:54:37 -0700

    CA1801

--- DelegatedAppCreator/GlobalSuppressions.cs
+++ DelegatedAppCreator/GlobalSuppressions.cs
@@ -7,3 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "English only. No need for a resource table.", Scope = "member", Target = "~M:DelegatedAppCreator.DelegatedAppCreator.Main(System.String[])~System.Threading.Tasks.Task")]
 [assembly: SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "English only. No need for a resource table.", Scope = "member", Target = "~M:DelegatedAppCreator.DelegatedAppCreator.PrintApplicationNames(System.String,System.Collections.Generic.IEnumerable{System.String})")]
+[assembly: SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Main method args are unused.", Scope = "member", Target = "~M:DelegatedAppCreator.DelegatedAppCreator.Main(System.String[])~System.Threading.Tasks.Task")]
