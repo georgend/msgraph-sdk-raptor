cf824d0e0279debce2032ef35049a2c40a249d70
George <1641829+georgend@users.noreply.github.com>
2021-09-23 22:46:40 +0300

    Add TokenLifetimePolicy data. (#475)
    * Add TokenLifetimePolicy data.
    
    * Add FeatureRolloutPolicy data.
    
    * Refactor script to introduce variables for urls.

--- scripts/TenantSetup/TenantHttpData/Policy/Policy.ps1
+++ scripts/TenantSetup/TenantHttpData/Policy/Policy.ps1
@@ -14,48 +14,77 @@ Connect-MgGraph -CertificateThumbprint $appSettings.CertificateThumbprint -Clien
 
 #Create activityBasedTimeoutPolicy https://docs.microsoft.com/en-us/graph/api/activitybasedtimeoutpolicy-post-activitybasedtimeoutpolicies?view=graph-rest-1.0&tabs=http
 $activityBasedTimeoutPolicyData = Get-RequestData -ChildEntity "ActivityBasedTimeoutPolicy"
-$currentActivityBasedTimeoutPolicyPolicy = Invoke-RequestHelper -Uri "policies/activityBasedTimeoutPolicies" -Method GET |
+$activityBaseTimeoutPolicyUrl = "policies/activityBasedTimeoutPolicies"
+$currentActivityBasedTimeoutPolicyPolicy = Invoke-RequestHelper -Uri $activityBaseTimeoutPolicyUrl -Method GET |
         Where-Object { $_.displayName -eq $activityBasedTimeoutPolicyData.displayName } |
         Select-Object -First 1
 
 if($null -eq $currentActivityBasedTimeoutPolicyPolicy){
-    $currentActivityBasedTimeoutPolicyPolicy = Invoke-RequestHelper -Uri "policies/activityBasedTimeoutPolicies" -Method POST -Body $activityBasedTimeoutPolicyData
+    $currentActivityBasedTimeoutPolicyPolicy = Invoke-RequestHelper -Uri $activityBaseTimeoutPolicyUrl -Method POST -Body $activityBasedTimeoutPolicyData
     $currentActivityBasedTimeoutPolicyPolicy.id
 }
 
 $claimsMappingPolicyData = Get-RequestData -ChildEntity "ClaimsMappingPolicy"
-$currentClaimsMappingPolicy = Invoke-RequestHelper -Uri "policies/claimsMappingPolicies" -Method GET |
+$claimMappingPolicyUrl = "policies/claimsMappingPolicies"
+$currentClaimsMappingPolicy = Invoke-RequestHelper -Uri $claimMappingPolicyUrl -Method GET |
         Where-Object { $_.displayName -eq $claimsMappingPolicyData.displayName } |
         Select-Object -First 1
 
 if($null -eq $currentClaimsMappingPolicy){
-    $currentClaimsMappingPolicy = Invoke-RequestHelper -Uri "policies/claimsMappingPolicies" -Method POST -Body $claimsMappingPolicyData
+    $currentClaimsMappingPolicy = Invoke-RequestHelper -Uri $claimMappingPolicyUrl -Method POST -Body $claimsMappingPolicyData
     $currentClaimsMappingPolicy.id
 }
 
 $homeRealmDiscoveryPolicyData = Get-RequestData -ChildEntity "HomeRealmDiscoveryPolicy"
-$currentHomeRealmDiscoveryPolicyData = Invoke-RequestHelper -Uri "policies/homeRealmDiscoveryPolicies" -Method GET |
+$homeRealDiscoveryPolcyUrl = "policies/homeRealmDiscoveryPolicies"
+$currentHomeRealmDiscoveryPolicyData = Invoke-RequestHelper -Uri $homeRealDiscoveryPolcyUrl -Method GET |
         Where-Object { $_.displayName -eq $homeRealmDiscoveryPolicyData.displayName } |
         Select-Object -First 1
 
 if($null -eq $currentHomeRealmDiscoveryPolicyData){
-    $currentHomeRealmDiscoveryPolicyData = Invoke-RequestHelper -Uri "policies/homeRealmDiscoveryPolicies" -Method POST -Body $homeRealmDiscoveryPolicyData
+    $currentHomeRealmDiscoveryPolicyData = Invoke-RequestHelper -Uri $homeRealDiscoveryPolcyUrl -Method POST -Body $homeRealmDiscoveryPolicyData
     $currentHomeRealmDiscoveryPolicyData.id
 }
 
 $tokenIssuancePolicyData = Get-RequestData -ChildEntity "TokenIssuancePolicy"
-$currentTokenIssuancePolicy = Invoke-RequestHelper -Uri "policies/tokenIssuancePolicies" -Method GET |
+$tokenIssuancePolicyUrl = "policies/tokenIssuancePolicies"
+$currentTokenIssuancePolicy = Invoke-RequestHelper -Uri $tokenIssuancePolicyUrl -Method GET |
         Where-Object { $_.displayName -eq $tokenIssuancePolicyData.displayName } |
         Select-Object -First 1
 
 if($null -eq $currentTokenIssuancePolicy){
-    $currentTokenIssuancePolicy = Invoke-RequestHelper -Uri "policies/tokenIssuancePolicies" -Method POST -Body $tokenIssuancePolicyData
+    $currentTokenIssuancePolicy = Invoke-RequestHelper -Uri $tokenIssuancePolicyUrl -Method POST -Body $tokenIssuancePolicyData
     $currentTokenIssuancePolicy.id
+}
+#Create tokenLifetimePolicy https://docs.microsoft.com/en-us/graph/api/tokenlifetimepolicy-post-tokenlifetimepolicies?view=graph-rest-1.0&tabs=http
+$tokenLifetimePolicyData = Get-RequestData -ChildEntity "TokenLifetimePolicy"
+$tokenLifeTimePolicyUrl = "policies/tokenLifetimePolicies"
+$currentTokenLifetimePolicy = Invoke-RequestHelper -Uri $tokenLifeTimePolicyUrl -Method GET |
+        Where-Object { $_.displayName -eq $tokenLifetimePolicyData.displayName } |
+        Select-Object -First 1
+
+if($null -eq $currentTokenLifetimePolicy){
+    $currentTokenLifetimePolicy = Invoke-RequestHelper -Uri $tokenLifeTimePolicyUrl -Method POST -Body $tokenLifetimePolicyData
+    $currentTokenLifetimePolicy.id
+}
+
+#Create featureRolloutPolicy https://docs.microsoft.com/en-us/graph/api/featurerolloutpolicies-post?view=graph-rest-1.0&tabs=http
+$featureRolloutPolicyData = Get-RequestData -ChildEntity "FeatureRolloutPolicy"
+$featureRolloutPolicyUrl = "policies/featureRolloutPolicies"
+$currentFeatureRolloutPolicy = Invoke-RequestHelper -Uri $featureRolloutPolicyUrl -Method GET |
+        Where-Object { $_.displayName -eq $featureRolloutPolicyData.displayName } |
+        Select-Object -First 1
+
+if($null -eq $currentFeatureRolloutPolicy){
+    $currentFeatureRolloutPolicy = Invoke-RequestHelper -Uri $featureRolloutPolicyUrl -Method POST -Body $featureRolloutPolicyData
+    $currentFeatureRolloutPolicy.id
 }
 
 $identifiers.activityBasedTimeoutPolicy._value = $currentActivityBasedTimeoutPolicyPolicy.id
 $identifiers.claimsMappingPolicy._value = $currentClaimsMappingPolicy.id
 $identifiers.homeRealmDiscoveryPolicy._value = $currentHomeRealmDiscoveryPolicyData.id
 $identifiers.tokenIssuancePolicy._value = $currentHomeRealmDiscoveryPolicyData.id
+$identifiers.tokenLifetimePolicy._value = $currentTokenLifetimePolicy.id
+$identifiers.featureRolloutPolicy._value = $currentFeatureRolloutPolicy.id
 
 $identifiers | ConvertTo-Json -Depth 10 > $identifiersPath
