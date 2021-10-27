6cc937a266b19f8c1e226fb4a525b6f14f579536
Mustafa Zengin <muzengin@microsoft.com>
2021-10-27 01:13:00 +0000

    add manager creation script

---
+++ scripts/TenantSetup/TenantHttpData/manager/manager.ps1
@@ -0,0 +1,16 @@
+# Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+Param(
+    [string] $IdentifiersPath = (Join-Path $PSScriptRoot "../../../../msgraph-sdk-raptor-compiler-lib/identifiers.json" -Resolve)
+)
+
+$raptorUtils = Join-Path $PSScriptRoot "../../RaptorUtils.ps1" -Resolve
+. $raptorUtils
+
+$identifiers = Get-CurrentIdentifiers -IdentifiersPath $IdentifiersPath
+
+$userId = $identifiers.user._value;
+
+# assign same user as the manager (allowed)
+$managerRequest = "{`"@odata.id`": `"https://graph.microsoft.com/v1.0/users/$userId`"}"
+
+Request-DelegatedResource -Uri "me/manager/`$ref" -Method "PUT" -Body $managerRequest 
